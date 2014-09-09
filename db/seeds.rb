33bd9848ed2f57da8cf3ab124150c9b982b31e32
pablofullana <pablofullana@gmail.com>
2014-09-08 23:48:46 -0300

    Removed final '.' from seeded phrases

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ puts "Creating Phrases..."
 
 rand(100...200).times do
   Phrase.create(
-    content:  Faker::Lorem.sentence,
+    content:  Faker::Lorem.sentence[0..-2],
     author:   Faker::Name.name,
     language: 'Lorem'
   )
